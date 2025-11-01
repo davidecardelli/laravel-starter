7d867d549d7d5225a4eb96a0fadb5b0ffbf7f30f
davide-cardelli <davide.cardelli@mediass.it>
2025-11-01 20:14:42 +0100

    fix(tests): ensure factory generates valid phone numbers
    The UserFactory was using fake()->phoneNumber() which can generate
    phone numbers with invalid characters (x, dots, etc.) that don't match
    the validation regex: /^[+]?[0-9\s\-()]+$/
    
    Changed to fake()->numerify('+## (###) ###-####') which always
    generates valid phone numbers in the format: +XX (XXX) XXX-XXXX
    
    This fixes the 3 failing tests in UserManagementTest:
    - user roles can be changed during update
    - all user roles can be removed during update
    - user without roles parameter keeps existing roles

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -38,7 +38,7 @@ class UserFactory extends Factory
         return [
             'first_name' => fake()->firstName(),
             'last_name' => fake()->lastName(),
-            'phone' => fake()->phoneNumber(),
+            'phone' => fake()->numerify('+## (###) ###-####'),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= 'password',
